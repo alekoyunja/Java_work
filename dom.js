a38609d05bb30661f0cd01784aab8add173b5691
alekoyunja <alikoyuncu13@gmail.com>
2023-01-31 23:27:22 +0300

    Elementleri bulma

---
+++ dom.js
@@ -0,0 +1,27 @@
+// // // document.getElementById("bio").innerHTML="Ali KOYUNCU : 2023"
+
+// var intro1 = document.getElementById("intro1");
+
+// var mesaj = document.getElementById("mesaj");
+
+// mesaj.innerHTML = intro1.innerHTML;
+
+// var tumListeler = document.getElementsByTagName("ul");
+// var sehirler = tumListeler[0]
+
+// tumElemanlar = sehirler.getElementsByTagName("li");
+
+// for (i = 0; i < tumElemanlar.length; i++) {
+//     alert(tumElemanlar[i].innerHTML);
+  
+// }
+
+
+
+// classname konusu 
+
+var classElemanlari=document.getElementsByClassName("intro1");
+
+alert(classElemanlari[0].innerHTML)
+
+
---
+++ donguler.js
@@ -0,0 +1,15 @@
+// for ( i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+var sehirler=["Ankara","İstanbul","İzmir","Bursa"]
+
+for(i=0;i<=sehirler.length;i++){
+    console.log(sehirler[i])
+
+}
+
+do{
+    console.log(i)
+}while(i>10)
+
